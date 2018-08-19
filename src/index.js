bb7cf80dfde91bcbdbff267e91b2fc2f73deb2a0
ext.joao.silva.lopes.mindera <joao.silva.lopes@mindera.com>
2018-08-19 16:21:59 +0100

    RenderProps Pattern Example

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component {
+class Input extends React.Component {
+  
+  state = { value: 0 };
+  
   render() {
-    return <div/>;
+    const { value } = this.state;
+    return (
+      <div>
+        <input
+          value = { value }
+          onChange = { ({ target : { value } }) => this.setState({ value }) }
+        />
+        { this.props.render(value) }
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Input render={(n)=><span>{n**2}</span>}/>,
   document.getElementById('app')
 );
